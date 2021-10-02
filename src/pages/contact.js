2952bf049f7c07b933fa408a8ed67e5f706c0739
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-10-03 00:57:17 +0800

    Update contents and styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,16 @@ export default function Contact() {
       <p>
         I'm looking for any new opportunities. If you're interested in working
         with me, or you want to just say hi, feel free to send me an{" "}
-        <a href="mailto:pohanc649@gmail.com">email</a> 🙂.
+        <a href="mailto:pohanc649@gmail.com">email</a>, or send me a message
+        with{" "}
+        <a
+          href="https://www.linkedin.com/in/柏翰-陳-78a95021a"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>{" "}
+        🙂.
       </p>
     </PageLayout>
   );
