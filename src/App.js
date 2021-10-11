3531c6d7451fa3387a6fea45086fbd769dbc34f2
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-10-11 09:30:59 +0800

    Use React.lazy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styles from "./App.module.css";
 import Navigation from "./components/Navigation";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Contact from "./pages/contact";
+const About = React.lazy(() => import("./pages/about"));
+const Projects = React.lazy(() => import("./pages/projects"));
+const Contact = React.lazy(() => import("./pages/contact"));
 
 export default function App() {
   return (
@@ -12,9 +13,11 @@ export default function App() {
         <Navigation />
 
         <Switch>
-          <Route path="/" exact component={About} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
+          <Suspense fallback="Loading...">
+            <Route path="/" exact component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </Suspense>
         </Switch>
       </div>
     </Router>
