2ff1d18e10d10842a5828620756c48d1cbc09305
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2022-01-01 08:50:35 +0800

    Shut down the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import styles from "./App.module.css";
-import Navigation from "./components/Navigation";
-const About = React.lazy(() => import("./pages/about"));
-const Projects = React.lazy(() => import("./pages/projects"));
-const Contact = React.lazy(() => import("./pages/contact"));
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import styles from "./App.module.css";
+// import Navigation from "./components/Navigation";
+// const About = React.lazy(() => import("./pages/about"));
+// const Projects = React.lazy(() => import("./pages/projects"));
+// const Contact = React.lazy(() => import("./pages/contact"));
 
 export default function App() {
   return (
@@ -15,11 +15,11 @@ export default function App() {
           <Navigation />
 
           <Switch>
-            <Suspense fallback="Loading...">
+            <React.Suspense fallback="Loading...">
               <Route path="/" exact component={About} />
               <Route path="/projects" component={Projects} />
               <Route path="/contact" component={Contact} />
-            </Suspense>
+            </React.Suspense>
           </Switch>
         </div>
       </Router> */}
